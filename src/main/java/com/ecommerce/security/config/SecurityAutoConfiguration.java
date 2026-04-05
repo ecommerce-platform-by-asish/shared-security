ba9620030c199929f01feeb2ffa14baa9883586e
asish <beingasishkumar@gmail.com>
2026-04-05 11:15:50 +0530

    reactive configuration added

--- src/main/java/com/ecommerce/security/config/SecurityAutoConfiguration.java
+++ src/main/java/com/ecommerce/security/config/SecurityAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.ecommerce.security.config;
 
+import com.ecommerce.security.gateway.AuthenticationFilter;
 import com.ecommerce.security.jwt.JwtProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -12,8 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 /**
  * Main auto-configuration class for the shared-security library.
  *
- * <p>This class bootstraps both common beans (like the JWT Validator) and web-specific security
- * (like the UserContextFilter) when the library is detected on the classpath.
+ * <p>This class bootstraps common beans (like the JwtProvider) and conditionally imports the
+ * appropriate security configuration (Servlet or Reactive) based on the application type.
  */
 @Configuration
 public class SecurityAutoConfiguration {
@@ -38,4 +39,23 @@ public class SecurityAutoConfiguration {
   @ConditionalOnClass(name = "jakarta.servlet.DispatcherType")
   @Import(WebSecurityAutoConfiguration.class)
   static class WebSecurityImportConfig {}
+
+  /** Only loads if the Reactive (WebFlux) API is present. */
+  @Configuration
+  @ConditionalOnClass(name = "org.springframework.web.reactive.DispatcherHandler")
+  @Import(WebSecurityAutoConfiguration.class)
+  static class ReactiveSecurityImportConfig {}
+
+  /** Loads Gateway-specific security components if the Spring Cloud Gateway API is present. */
+  @Configuration
+  @ConditionalOnClass(
+      name = "org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory")
+  static class GatewaySecurityConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AuthenticationFilter authenticationFilter(JwtProvider jwtProvider) {
+      return new AuthenticationFilter(jwtProvider);
+    }
+  }
 }
